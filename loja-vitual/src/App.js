e227422f657238b56ea1359d8c02fa4405b05bbb
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-04-18 10:55:00 -0300

    redux configurado

--- loja-vitual/src/App.js
+++ loja-vitual/src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 import './App.css';
-import { Button } from 'reactstrap';
-
-function App() {
-  return (
-    <div>
-        
+class App extends Component {
+  state = {
+    inputValue: ''
+  }
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+  render() {
+    const {
+      clickButton,
+      newValue
+    } = this.props;
+    const { inputValue } = this.state;
+    return (
+      <div className="App" style={{ paddingTop: '10px' }}>
+        <input
+          onChange={this.inputChange}
+          type='text'
+          value={inputValue}
+        />
+        <button onClick={() => clickButton(inputValue)}>
+          Click me!
+        </button>
+        <h1>{newValue}</h1>
       </div>
-  );
+    );
+  }
 }
-
-export default App;
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickButton }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
