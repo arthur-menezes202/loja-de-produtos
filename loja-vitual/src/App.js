d581b92918fdcc039aea9b6cf19c25523f86f735
arthur-menezes202 <arthurmcorrea282@gmail.com>
2022-04-18 10:33:16 -0300

    configuração e instalaçao dos pacotes do projeto finalizados, agora vou começar a configurar o redux

---
+++ loja-vitual/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div>
+        
+      </div>
+  );
+}
+
+export default App;
